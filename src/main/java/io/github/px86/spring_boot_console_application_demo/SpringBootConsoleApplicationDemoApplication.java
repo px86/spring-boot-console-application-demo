b140d83f4ac62f661e077a5cc0115192c2c801b9
Pushkar Raj <px86@protonmail.com>
2024-08-17 12:31:04 +0530

    Ran google-java-format

--- src/main/java/io/github/px86/spring_boot_console_application_demo/SpringBootConsoleApplicationDemoApplication.java
+++ src/main/java/io/github/px86/spring_boot_console_application_demo/SpringBootConsoleApplicationDemoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootConsoleApplicationDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootConsoleApplicationDemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootConsoleApplicationDemoApplication.class, args);
+  }
 }
