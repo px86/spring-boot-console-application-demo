c8e7596b53e4bffd9b4939de8ddbd0b3043a4e90
Pushkar Raj <px86@protonmail.com>
2024-08-17 12:27:16 +0530

    Initial commit

---
+++ src/main/java/io/github/px86/spring_boot_console_application_demo/SpringBootConsoleApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.px86.spring_boot_console_application_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsoleApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConsoleApplicationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/px86/spring_boot_console_application_demo/SpringBootConsoleApplicationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.px86.spring_boot_console_application_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConsoleApplicationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
